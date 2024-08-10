d2fbceffa222c72390c49cc055959207c19e8bc2
Shhhamsy <56039112+Shamsyyy@users.noreply.github.com>
2024-08-10 22:07:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+pet_type = input("Введите вид питомца: ")
+pet_age = input("Введите возраст питомца: ")
+pet_name = input("Введите кличку питомца: ")
+print(f"это {pet_type} по кличке \"{pet_name}\". возраст {pet_age} года")
